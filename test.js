f054652d10b448539d19cfc7f34de6144b585db4
Gautam Amin <gootdude@gmail.com>
2011-12-17 17:47:27 -0600

    First commit

---
+++ test.js
@@ -0,0 +1,13 @@
+/**
+ * Created by JetBrains WebStorm.
+ * User: Gautam Amin
+ * Date: 12/17/11
+ * Time: 5:22 PM
+ * To change this template use File | Settings | File Templates.
+ */
+x = 5;
+setTimeout(function () {
+  debugger;
+  console.log("world");
+}, 1000);
+console.log("hello");
